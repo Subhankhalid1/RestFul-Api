e356944e54035651194b94ee249332f6acbceb3c
Subhankhalid1 <rsubhakhalid@gmail.com>
2020-11-28 00:49:36 +0500

    update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ import React from 'react'
 const SearchBar=(props)=>{
     return(
         <>
-        <input value={props.value}
+        <input value={props.value} style={{width:"360px", margin:"1%"}}
          onChange={(event)=>props.setSearchValue(event.target.value)}
           placeholder="Type to search">
 
